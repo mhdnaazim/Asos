3a6613eaa0781fe7ae42ce1fa707be300e035526
mhdnaazim <mohdnaazim252@gmail.com>
2025-10-31 15:47:15 +0530

    Font

--- Frontend/src/User/Men/MiniNav.jsx
+++ Frontend/src/User/Men/MiniNav.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import './MiniNav.css';
+import india from '../../assets/in.png'
 
 const MiniNav = () => {
     return (
         <>
         <div className="miniNav-container">
             <div className="miniNav-contents">
-                <div className="miniNav-contents-left"></div>
-                <div className="miniNav-contents-right"></div>
+                <div className="miniNav-contents-left">
+                    <p>Help & FAQs</p>
+                </div>
+                <div className="miniNav-contents-right">
+                    <img src={india}/>
+                </div>
             </div>
         </div>
         </>
