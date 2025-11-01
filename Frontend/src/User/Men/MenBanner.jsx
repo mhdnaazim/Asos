3ddfb7f571d1356403006ca9aca35fbb3acabc58
mhdnaazim <mohdnaazim252@gmail.com>
2025-11-01 18:33:27 +0530

    Men Banner

---
+++ Frontend/src/User/Men/MenBanner.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './MenBanner.css'
+
+const MenBanner = () => {
+  return (
+    <>
+      <div className="men-banner"></div>
+    </>
+  )
+}
+
+export default MenBanner
--- Frontend/src/User/Men/MenLanding.jsx
+++ Frontend/src/User/Men/MenLanding.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Nav from "./Nav";
+import MenBanner from "./MenBanner";
 
 const MenLanding = () => {
     return(
         <>
         <Nav />
+        <MenBanner />
         </>
     )
 }
