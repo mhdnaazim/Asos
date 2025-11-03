87413794adaafb6064867053e48019e9328a5422
mhdnaazim <mohdnaazim252@gmail.com>
2025-11-03 17:23:08 +0530

    Women Initial

---
+++ Frontend/src/User/Women/WomenBanner.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './WomenBanner.css'
+
+const WomenBanner = () => {
+  return (
+    <>
+      <div className="women-banner"></div>
+    </>
+  )
+}
+
+export default WomenBanner
--- Frontend/src/User/Women/WomenLanding.jsx
+++ Frontend/src/User/Women/WomenLanding.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import WomenNav from './WomenNav';
+import WomenBanner from './WomenBanner';
 
 const WomenLanding = () => {
   return (
     <>
       <WomenNav />
+      <WomenBanner />
     </>
   )
 }
